4017f83d404c62f0239556c37930858dd7042664
raphaelbachega <53584067+raphaelbachega@users.noreply.github.com>
2019-09-29 20:14:18 -0300

    attTalmid

---
+++ conteudoBandtecBachega/siteIndividualTalmid/script02.js
@@ -0,0 +1,21 @@
+function selecionar() {
+    if (intuito.value == 0) {
+       div_cadastro.style.display='none';
+    }
+    if (intuito.value == 1) {
+       a1.href = `https://www.google.com`;
+       div_cadastro.style.display='block';
+    }
+    if (intuito.value == 2) {
+       a1.href = `https://pt-br.facebook.com`;
+       div_cadastro.style.display='block';
+    }
+    if (intuito.value == 3) {
+       a1.href = `https://www.linkedin.com`;
+       div_cadastro.style.display='block';
+    }
+    if (intuito.value == 4) {
+       a1.href = `https://www.googledrive.com`;
+       div_cadastro.style.display='block';
+    }
+   }
