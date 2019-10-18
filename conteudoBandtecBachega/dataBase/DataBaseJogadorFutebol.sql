f819a727b37976c423a9c7f76a379d57b465b88e
Aluno <raphael.messias@bandtec.com.br>
2019-10-17 21:09:57 -0300

    attDB

--- conteudoBandtecBachega/dataBase/DataBaseJogadorFutebol.sql
+++ conteudoBandtecBachega/dataBase/DataBaseJogadorFutebol.sql
@@ -26,3 +26,21 @@ insert into futtime values
 (null,'Cruzeiro','Rogério Ceni',19210102);
 select * from futtime;
 select * from jogador;
+
+insert into jogador values
+(100,'Meio Campo','Bruno Henrique',983676428,1,null),
+(101,'Atacante','Fred',938475683,5,null),
+(102,'Zagueiro','Gustavo henrique',76543456,4,null),
+(103,'Goleiro','Douglas Friedrich',948572385,3,null),
+(104,'Lateral','Diogo Barbosa',765849300,1,null),
+(105,'Ponta Direita','Antony',958711209,2,null);
+
+update jogador set fkjogador = 102 where idJogador = 100;
+update jogador set fkjogador = 101 where idJogador = 103;
+update jogador set fkjogador = 104 where idJogador = 105;
+update jogador set fkjogador = 102 where idJogador = 100;
+update jogador set fkjogador = 102 where idJogador = 100;
+
+
+
+
