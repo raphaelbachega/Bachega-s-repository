9d2bc2f44e5c8187c74775d7b0176d3c6b2107d1
Aluno Bandtec <raphael.messias@bandtec.com.br>
2019-12-02 12:25:12 -0200

    attSite

--- conteudoBandtecBachega/siteTalmid/talmidDataBase.sql
+++ conteudoBandtecBachega/siteTalmid/talmidDataBase.sql
@@ -4,6 +4,6 @@ use talmid;
 create table usuarios(
 id_usuarios int primary key auto_increment,
 nome varchar(45),
-login varchar(45),
+login_usuario varchar(45),
 senha varchar(45),
 interesse varchar(45))auto_increment = 1;
